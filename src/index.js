68bc3820df44db78d262b2085d971479fac94db7
Format73 <matteo.forti@gmail.com>
2025-03-14 14:08:14 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,11 +6,6 @@
 import { registerBlockType } from '@wordpress/blocks';
 
 /**
-* Import per internazionalizzazione
-*/
-import { __, sprintf } from '@wordpress/i18n';
-
-/**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
  * All files containing `style` keyword are bundled together. The code used
  * gets applied both to the front of your site and to the editor.
@@ -21,34 +16,18 @@ import './style.scss';
 
 /**
  * Internal dependencies
- * import di save non serve per blocchi dinamici
  */
 import Edit from './edit';
-//import save from './save';
-
-/**
- * Import from json
- */
 import metadata from './block.json';
-const { name, ...settings } = metadata;
-
 
 /**
  * Every block starts by registering a new block type definition.
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
- * 
- * export di save non serve per blocchi dinamici
  */
-registerBlockType(name, {
-	...settings,
+registerBlockType( metadata.name, {
 	/**
 	 * @see ./edit.js
 	 */
 	edit: Edit,
-
-	/**
-	 * @see ./save.js
-	 */
-	// save,
-});
+} );
