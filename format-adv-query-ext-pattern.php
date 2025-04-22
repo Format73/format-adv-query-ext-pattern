18c0ab61a18a6595b709e6c077a135ec9e0a6276
Format73 <matteo.forti@gmail.com>
2025-04-22 08:44:10 +0200

    Add files via upload

--- format-adv-query-ext-pattern.php
+++ format-adv-query-ext-pattern.php
@@ -48,11 +48,11 @@ final class FM_ADVQ_EP {
 		add_action('admin_enqueue_scripts', array($this,'FM_pass_parameters_to_block'));
 		add_action('wp_enqueue_scripts', array($this,'FM_pass_parameters_to_block'));
 		
-		//aggiungo filtro per gestione css dinamica
+		//aggiungo azione per css dinamico nel backend
+		add_action('enqueue_block_editor_assets', array ($this,'FM_carica_css_dinamico_gutenberg')) ;
 		
+		//aggiungo filtro per gestione css dinamica nel frontend
 		add_filter ('render_block_format-advq-ep/fm-ql-show',  array($this,'FM_load_dynamic_css'),10,2);
-		
-		
 		
 		//registro hook disattivazione
 		register_deactivation_hook( __FILE__ , [$this, 'deactivate_plugin' ]);
@@ -104,49 +104,8 @@ final class FM_ADVQ_EP {
 	public function FM_load_dynamic_css ( $block_content, $block ){
 		//recupero nome del template usato
 		if ((isset ($block['attrs']['Visualizzazione'])) && ($block['attrs']['Visualizzazione']!='')){
-			$path_parts = pathinfo($block['attrs']['Visualizzazione']);
-			$nome_file = $path_parts['filename'];
 			
-			$local_path=rtrim(ABSPATH,'/');
-			
-			$dir_file = $local_path.$path_parts['dirname'].'/';
-			$file_css=$nome_file.'.css';
-			$file_min_css=$nome_file.'.min.css';
-			
-			$template_base_dir = 'fm_advq_patterns/';
-			$template_plugin_dir =plugin_dir_path(__FILE__) . $template_base_dir  ;
-			$template_plugin_url = plugins_url( '', __FILE__ ) . '/'. $template_base_dir  ;
-			$template_theme_dir = get_template_directory() .'/'.$template_base_dir ;
-			$template_theme_url = get_template_directory_uri() .'/'.$template_base_dir ;
-			
-			
-			//usato per registrare css
-			$handle_name="fm-dyn-style-".$nome_file;
-			
-			//verifico se template locale o nel tema 
-			if ($dir_file==$template_plugin_dir){
-				$base_url=$template_plugin_url;
-			}
-			elseif ($dir_file==$template_theme_dir){
-				$base_url=$template_theme_url;
-			}
-			else {
-				$base_url='';
-			}
-			
-			//verifico se ho file css o min.css ed eventualmente lo metto in coda
-			if (file_exists($dir_file.$file_min_css)){
-				if ($base_url!=''){
-					wp_enqueue_style( $handle_name, $base_url.$file_min_css );	
-				}
-
-			}
-			elseif (file_exists($dir_file.$file_css)){
-				if ($base_url!=''){
-					wp_enqueue_style( $handle_name, $base_url.$file_css );	
-				}
-
-			}
+			$this->FM_enqueue_css ($block['attrs']['Visualizzazione']);
 			
 		}
 		
@@ -205,6 +164,89 @@ final class FM_ADVQ_EP {
 		
 	}
 	
+	//funzione per caricare css dinamico in backend
+	public function FM_carica_css_dinamico_gutenberg(){
+		$current_screen = get_current_screen();
+		// Verifica che siamo nell'editor di post/pagine e non in altre aree di admin
+		if ( $current_screen && $current_screen->base === 'post' ) {
+			// Recupera l'ID del post attuale
+			$post_id = get_the_ID();
+			if (!$post_id) return;
+			// Recupera il contenuto dell'editor
+			$contenuto = get_post_field('post_content', $post_id);
+			// Verifica se il blocco specifico è presente
+			$nome_blocco='format-advq-ep/fm-ql-show';
+			if (has_block($nome_blocco)) {
+				$lista_blocchi = parse_blocks($contenuto);
+				array_walk_recursive($lista_blocchi, array($this,'FM_recursive_block'));
+					
+			}
+		}
+	}
+	
+	//funzione per recuperare i valori dei template usati dal blocco, richiamata sopra
+	private function FM_recursive_block ($value, $key){
+
+		if ($key=='Visualizzazione'){
+			if ($value !=''){
+				$this->FM_enqueue_css ($value);
+			}
+		}
+		
+	}
+	
+	//funzione per risolvere path css ed eventualmente includerlo, richiamata sia per frontend che backend
+
+	private function FM_enqueue_css($percorso_file){
+		if ($percorso_file==''){
+			return; 
+		}
+		
+		$path_parts = pathinfo($percorso_file);
+		$nome_file = $path_parts['filename'];
+		
+		$local_path=rtrim(ABSPATH,'/');
+		
+		$dir_file = $local_path.$path_parts['dirname'].'/';
+		$file_css=$nome_file.'.css';
+		$file_min_css=$nome_file.'.min.css';
+		
+		$template_base_dir = 'fm_advq_patterns/';
+		$template_plugin_dir =plugin_dir_path(__FILE__) . $template_base_dir  ;
+		$template_plugin_url = plugins_url( '', __FILE__ ) . '/'. $template_base_dir  ;
+		$template_theme_dir = get_template_directory() .'/'.$template_base_dir ;
+		$template_theme_url = get_template_directory_uri() .'/'.$template_base_dir ;
+		
+		
+		//usato per registrare css
+		$handle_name="fm-dyn-style-".$nome_file;
+		
+		//verifico se template locale o nel tema 
+		if ($dir_file==$template_plugin_dir){
+			$base_url=$template_plugin_url;
+		}
+		elseif ($dir_file==$template_theme_dir){
+			$base_url=$template_theme_url;
+		}
+		else {
+			$base_url='';
+		}
+		
+		//verifico se ho file css o min.css ed eventualmente lo metto in coda
+		if (file_exists($dir_file.$file_min_css)){
+			if ($base_url!=''){
+				wp_enqueue_style( $handle_name, $base_url.$file_min_css );	
+			}
+
+		}
+		elseif (file_exists($dir_file.$file_css)){
+			if ($base_url!=''){
+				wp_enqueue_style( $handle_name, $base_url.$file_css );	
+			}
+
+		}
+		
+	}
    
 }   
 
